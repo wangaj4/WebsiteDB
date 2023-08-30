c5f4b1b0f6870e0999affa922394c962deb3ab6b
Andrew Wang <spartan117aw@gmail.com>
2023-08-29 20:24:30 -0700

    recaptcha updates

--- cs122b-project2-login-cart-example-main/src/RecaptchaConstants.java
+++ cs122b-project2-login-cart-example-main/src/RecaptchaConstants.java
@@ -1,5 +1,5 @@
 
 public class RecaptchaConstants {
-    public static final String SECRET_KEY ="6LdA5dslAAAAAFv8hs7n8ocSOZZ8JaWJroEGpcfT";
+    public static final String SECRET_KEY ="6LeCMeYnAAAAAJOQrmIUpElVH1pmWvuIwhiw9fZ7";
 
 }
